539943d3801baf9689e79ecb1b1f365a3bc3d301
Oscar Elhanafey <oscar.elhanafey@gmail.com>
2022-04-03 13:45:42 -0400

    added GroceryItem model

--- grocery_backend/grocery_items/tests.py
+++ grocery_backend/grocery_items/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import GroceryItem
 
-# Create your tests here.
+class GroceryItemTestCase(TestCase):
+    def test_create_item(self):
+        # Ensure there are no items in db
+        items = GroceryItem.objects.all()
+        self.assertFalse(items)
+
+        # Create grocery item and check that it is created
+        GroceryItem.objects.create(name="Test Item", quantity=1, is_purchased=False)
+        item_count = GroceryItem.objects.filter(name="Test Item", quantity=1, is_purchased=False).count()
+        self.assertEqual(item_count, 1)
+
+
---
+++ grocery_backend/grocery_items/tests_api.py
@@ -0,0 +1,15 @@
+from rest_framework.test import APITestCase
+
+class GroceryItemAPITestCase(APITestCase):
+    def test_create_item(self):
+        pass
+    def test_get_all_items(self):
+        pass
+    def test_get_specific_item(self):
+        pass
+    def test_update_item_name_and_quantity(self):
+        pass
+    def test_set_item_puchased(self):
+        pass
+    def test_delete_item(self):
+        pass
